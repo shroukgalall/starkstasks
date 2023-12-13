49532f341f1f52f2ed434cda067ae3a36c857402
Shrouk Galal <153127437+shroukgalall@users.noreply.github.com>
2023-12-13 20:03:21 +0200

    Add files via upload

---
+++ file2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(void)
+
+{
+float n1,n2;
+printf("enter n1");
+scanf("%f",&n1);
+
+printf("enter n2");
+scanf("%f",&n2);
+
+printf("\n n1 = %f,  n2= %f",n1,n2);
+
+
+
+}

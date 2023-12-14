45003f909dc27f020fec1c9db69db3639cb3d13f
Shrouk Galal <153127437+shroukgalall@users.noreply.github.com>
2023-12-14 09:44:56 +0200

    file2.c

--- file2.c
+++ file2.c
@@ -3,15 +3,18 @@
 void main(void)
 
 {
-float n1,n2;
+float n1;
+float n2;
 printf("enter n1");
 scanf("%f",&n1);
 
 printf("enter n2");
 scanf("%f",&n2);
 
-printf("\n n1 = %f,  n2= %f",n1,n2);
-
+printf("\n befor swiping : \nn1 = %f\nn2= %f\n",n1,n2);
+float z=n1 ;
+float x=n2;
+printf("\n after swiping : \nn1 = %f\nn2= %f\n",x,z);
 
 
 }
